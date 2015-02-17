901eebeaa322f35036735a6ffa35a01fba029bfe
Claudio Sanches <contato@claudiosmweb.com>
2015-02-17 19:16:12 -0200

    Save the discount in the order data

--- includes/class-wc-payment-discounts-add-discount.php
+++ includes/class-wc-payment-discounts-add-discount.php
@@ -38,8 +38,8 @@ class WC_Payment_Discounts_Add_Discount {
 		// Display the discount in payment gateways titles.
 		add_filter( 'woocommerce_gateway_title', array( $this, 'gateway_title' ), 10, 2 );
 
-		// Fix salved payment method title.
-		add_action( 'woocommerce_checkout_order_processed', array( $this, 'fix_payment_method_title' ), 1 );
+		// Fix salved payment method title and update the cart discount total.
+		add_action( 'woocommerce_checkout_order_processed', array( $this, 'update_order_data' ), 10 );
 	}
 
 	/**
@@ -167,18 +167,18 @@ class WC_Payment_Discounts_Add_Discount {
 	}
 
 	/**
-	 * Fix payment method title.
-	 * Remove the discount in the title.
+	 * Remove the discount in the title and update the cart discount total.
 	 *
-	 * @param int  $order_id Order ID.
-	 * @param array $posted  Posted order data.
+	 * @param int $order_id Order ID.
 	 */
-	public function fix_payment_method_title( $order_id ) {
-		$old_title = get_post_meta( $order_id, '_payment_method_title', true );
-		$new_title = preg_replace( '/<small>.*<\/small>/', '', $old_title );
+	public function update_order_data( $order_id ) {
+		$old_title    = get_post_meta( $order_id, '_payment_method_title', true );
+		$new_title    = preg_replace( '/<small>.*<\/small>/', '', $old_title );
+		$old_discount = get_post_meta( $order_id, '_cart_discount', true );
 
 		// Save the fixed title.
 		update_post_meta( $order_id, '_payment_method_title', $new_title );
+		update_post_meta( $order_id, '_cart_discount', $this->cart_discount + $old_discount );
 	}
 }
 
